ca896d9e5040ca198aae9e8f91ff74c02b456fb4
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-03-25 21:16:40 +0300

    Admin Layout has been created.

---
+++ Frontend/TrackingProject.WebUI/Controllers/AdminLayout/AdminLayoutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrackingProject.WebUI.Controllers.AdminLayout
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult AdminLayout()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
