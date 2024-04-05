c135c4357b3d4b781a0d9d600280664bf32fec76
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-04-05 20:57:43 +0300

    AutoMapper Config file added.

---
+++ ApiConsume/TrackingProject.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TrackingProject.DtoLayer.Dtos.AnnouncementDto;
+using TrackingProject.DtoLayer.Dtos.PanelUserDto;
+using TrackingProject.EntityLayer.Concrete;
+
+namespace TrackingProject.WebApi.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<ResultAnnouncementDto, Announcement>().ReverseMap();
+            CreateMap<CreateAnnouncementDto, Announcement>().ReverseMap();
+            CreateMap<UpdateAnnouncementDto, Announcement>().ReverseMap();
+            
+            //CreateMap<RegisterPanelUserDto, PanelUser>().ReverseMap();
+            //CreateMap<ResultPanelUserDto, PanelUser>().ReverseMap();
+            //CreateMap<UpdatePanelUserDto, PanelUser>().ReverseMap();
+
+        }
+    }
+}
