3f903b3668ad3937e17542ae157e7f157f5ba3de
Ahmet Seyyit Köse <21110131061@ogr.ksu.edu.tr>
2024-04-06 11:15:30 +0300

    Various changes with master branch.

---
+++ ApiConsume/TrackingProject.DtoLayer/Dtos/EmployeeDto/LoginEmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingProject.DtoLayer.Dtos.EmployeeDto
+{
+    public class LoginEmployeeDto
+    {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50)]
+        [MinLength(5)]
+        public string Password { get; set; }
+    }
+}
---
+++ ApiConsume/TrackingProject.DtoLayer/Dtos/PanelUserDto/LoginPanelUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackingProject.DtoLayer.Dtos.PanelUserDto
+{
+    public class LoginPanelUserDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
