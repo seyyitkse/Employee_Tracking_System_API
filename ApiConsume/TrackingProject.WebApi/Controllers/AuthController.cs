69860892e8c4e30fb3f745752cdf00934ce93113
Ahmet Seyyit Köse <21110131061@ogr.ksu.edu.tr>
2024-04-10 05:14:41 +0300

    All users in the ASP.NET User table are listed.

--- ApiConsume/TrackingProject.WebApi/Controllers/AuthController.cs
+++ ApiConsume/TrackingProject.WebApi/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using NuGet.Common;
 using System.Security.Claims;
 using TrackingProject.BusinessLayer.Abstract;
@@ -12,20 +13,58 @@ namespace TrackingProject.WebApi.Controllers
     public class AuthController : ControllerBase
     {
         private IEmployeeService _employeeService;
+        private readonly UserManager<IdentityUser > _userManager;
 
-        public AuthController(IEmployeeService employeeService)
+        public AuthController(IEmployeeService employeeService, UserManager<IdentityUser> userManager)
         {
             _employeeService = employeeService;
+            _userManager = userManager;
         }
 
+
+        //[HttpPost("Register")]
+        //public async Task<IActionResult> RegisterAsync([FromBody]CreateEmployeeDto model)
+        //{
+        //    if (ModelState.IsValid) 
+        //    {
+        //        var result=await _employeeService.RegisterUserAsync(model);
+        //        if (result.IsSuccess)
+        //        {
+        //            return Ok(result);
+        //        }
+        //        return BadRequest(result);
+        //    }
+        //    return BadRequest("Some properties are not valid");
+        //}
         [HttpPost("Register")]
-        public async Task<IActionResult> RegisterAsync([FromBody]CreateEmployeeDto model)
+        public async Task<IActionResult> RegisterAsync([FromBody] CreateEmployeeDto model)
         {
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
-                var result=await _employeeService.RegisterUserAsync(model);
+                var result = await _employeeService.RegisterUserAsync(model);
                 if (result.IsSuccess)
                 {
+                    // Yeni kullanıcı başarıyla kaydedildiğinde varsayılan rol atanması
+                    var user = await _userManager.FindByNameAsync(model.Email);
+                    if (user != null)
+                    {
+                        // Varsayılan rolü eklemek için rol adını kullanabilirsiniz
+                        var roleName = "Employee";
+
+                        // Kullanıcıya rolü atama
+                        var roleResult = await _userManager.AddToRoleAsync(user, roleName);
+                        if (!roleResult.Succeeded)
+                        {
+                            // Rol atama başarısız ise uygun bir işlem yapılabilir
+                            return BadRequest("Failed to assign default role to the user.");
+                        }
+                    }
+                    else
+                    {
+                        // Kullanıcı bulunamazsa uygun bir işlem yapılabilir
+                        return BadRequest("User not found after registration.");
+                    }
+
                     return Ok(result);
                 }
                 return BadRequest(result);
