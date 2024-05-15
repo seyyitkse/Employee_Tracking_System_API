3262316cb3f2f4e9b043a630f83da6ab937ae391
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-05-15 22:08:57 +0300

    Add new controller Added ApplicationUser controller

---
+++ ApiConsume/TrackingProject.WebApi/Controllers/ApplicationUserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrackingProject.EntityLayer.Concrete;
+
+namespace TrackingProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetApplicationUsers()
+        {
+            var users = _userManager.Users.ToList();
+            return Ok(users);
+        }
+        [HttpGet("Employee/{departmentId}")]
+        public async Task<IActionResult> GetStudentUsersByDepartment(int departmentId)
+        {
+            var studentUsers = await _userManager.GetUsersInRoleAsync("Employee");
+            var studentUsersInDepartment = studentUsers.Where(u => u.DepartmentID == departmentId).ToList();
+
+            return Ok(studentUsersInDepartment);
+        }
+        [HttpGet("Admin/{departmentId}")]
+        public async Task<IActionResult> GetTeacherUsersByDepartment(int departmentId)
+        {
+            var teacherUsers = await _userManager.GetUsersInRoleAsync("Admin");
+            var teacherUsersInDepartment = teacherUsers.Where(u => u.DepartmentID == departmentId).ToList();
+
+            return Ok(teacherUsersInDepartment);
+        }
+    }
+}
