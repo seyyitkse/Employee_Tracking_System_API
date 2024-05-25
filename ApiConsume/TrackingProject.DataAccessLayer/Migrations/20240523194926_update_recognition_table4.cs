61b2d4544d9937ccc7d6c77dedd8a29c7b2953b3
Ahmet Seyyit Köse <ahmetseyyit94@gmail.com>
2024-05-25 10:12:58 +0300

    Add `Entry` column to `RecognitionNotifications` and `Alerts` tables
    Added two new migrations `update_recognition_table4` and `update_table_alert` to introduce a new `Entry` column of type `tinyint(1)` to the `RecognitionNotifications` and `Alerts` tables respectively. This column is not nullable and defaults to `false`. Updated `ContextModelSnapshot.cs` to reflect these changes in the model builder configuration. Generated corresponding Designer and migration files, updating the `BuildTargetModel`, `Up`, and `Down` methods to handle the new `Entry` column.

---
+++ ApiConsume/TrackingProject.DataAccessLayer/Migrations/20240523194926_update_recognition_table4.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingProject.DataAccessLayer.Migrations
+{
+    public partial class update_recognition_table4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Entry",
+                table: "RecognitionNotifications",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Entry",
+                table: "RecognitionNotifications");
+        }
+    }
+}
---
+++ ApiConsume/TrackingProject.DataAccessLayer/Migrations/20240523195520_update_table_alert.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackingProject.DataAccessLayer.Migrations
+{
+    public partial class update_table_alert : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Entry",
+                table: "Alerts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Entry",
+                table: "Alerts");
+        }
+    }
+}
